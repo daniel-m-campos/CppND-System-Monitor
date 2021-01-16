60cddc6f7a5ea27d281c1eefe3faaefe282426e6
Daniel M Campos <daniel.m.campos@icloud.com>
2021-01-16 09:01:10 -0600

    Use iomanip

--- src/format.cpp
+++ src/format.cpp
@@ -1,18 +1,18 @@
 #include "format.h"
 
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 using std::string;
 
 string Format::ElapsedTime(long seconds) {
-  auto ToString = [](long digit) {
-    const string& s_digit = std::to_string(digit);
-    return digit < 10 ? "0" + s_digit : s_digit;
-  };
-
   auto hours = seconds / kSecondsInHour;
   auto minutes = (seconds / kSecondsInMinute) % kMinutesInHour;
   auto seconds_left = seconds % kSecondsInMinute;
-  return ToString(hours) + ":" + ToString(minutes) + ":" +
-         ToString(seconds_left);
+  std::ostringstream elapsed_time;
+  elapsed_time << std::setw(2) << std::setfill('0') << hours << ":"
+               << std::setw(2) << std::setfill('0') << minutes << ":"
+               << std::setw(2) << std::setfill('0') << seconds_left;
+  return elapsed_time.str();
 }
